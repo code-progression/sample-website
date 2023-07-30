50049bf7ad381e6fe1fa3eae35240664383b04ca
Edmar Dausan <edmardausan15@gmail.com>
2023-07-30 23:20:34 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const buttonEl = document.getElementById('follow-btn');
+
+function showName(name) {
+  console.log(name)
+  alert(name)
+}
+
+
+const profiles = [
+  {
+    name: 'jed',
+    age: 29
+  },
+  {
+    name: 'Daniel',
+    age: 17
+  },
+  {
+    name: 'ruel',
+    age: 24
+  }
+]
+
+console.log(buttonEl);
